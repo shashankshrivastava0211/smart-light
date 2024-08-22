a01ccafb97e718519f4f81d18aa86451979ada01
shashank shrivastava <internstudent02@gmail.com>
2024-08-22 18:48:27 +0530

    commit

--- src/components/EarthImage.tsx
+++ src/components/EarthImage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import image from "..//assets/images/earth-image.png";
 const EarthImage = () => {
   return (
--- src/components/icons/Icon2.tsx
+++ src/components/icons/Icon2.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Icon2 = () => {
   return (
     <>
